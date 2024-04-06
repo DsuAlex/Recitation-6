48783688b41f0f74d0ecc5fec0c3b4d8bf12d085
alexedwards <agedwards23@students.desu.edu>
2024-04-05 23:25:44 -0400

    hope this works!

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -84,9 +84,9 @@ public class TicTacToe {
         GamePiece winner = null;
 
         int[][] combos = {
-                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal winning combinations
-                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical winning combinations
-                {0, 4, 8}, {2, 4, 6}             // diagonal winning combinations
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                {0, 4, 8}, {2, 4, 6}
         };
 
         // TODO: add code here
